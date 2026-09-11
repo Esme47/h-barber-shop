a83c5ff4ba2d29378217e89659dcc8e1b03f9939
911 Nails <orteganini1@gmail.com>
2026-09-11 05:45:45 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,16 @@
+export const metadata = {
+  title: "H Barber Shop",
+  description: "Agenda de barbería",
+};
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
